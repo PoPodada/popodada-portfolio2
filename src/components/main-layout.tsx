5764c6e66a94830f4168cf8838092266144b4110
PoPodada <goito.722@icloud.com>
2025-08-13 00:57:31 +0900

    feat: view transition

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
 import { Boxes } from "@/components/ui/background-boxes";
 import { Button } from "@/components/ui/button";
+import { TransitionLink } from "./TransitionLink";
 
 const MainLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
 	return (
@@ -8,22 +8,25 @@ const MainLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
 			<div className="h-screen relative w-full overflow-hidden flex flex-col items-center justify-center rounded-lg">
 				<Boxes />
 				<div className="absolute top-20 z-20">
-					<Link className="text-white" href="/" passHref>
+					<TransitionLink href="/">
+						{/* className="text-white" */}
 						<Button
 							className="mr-2 ml-2 w-16 bg-[#171717]"
 							variant="outline"
 						>
 							top
 						</Button>
-					</Link>
-					<Link className="text-white" href="/posts" passHref>
+					</TransitionLink>
+
+					<TransitionLink href="/posts">
 						<Button
 							className="mr-2 ml-2 w-16 bg-[#171717]"
 							variant="outline"
 						>
 							posts
 						</Button>
-					</Link>
+					</TransitionLink>
+
 					{/* <Button
 						className="mr-2 ml-2 w-16 bg-[#171717]"
 						variant="outline"
