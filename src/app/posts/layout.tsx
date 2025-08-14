9439271b7f5b024ea2a1e251890b1fb334a197aa
PoPodada <goito.722@icloud.com>
2025-08-14 21:01:39 +0900

    fix: 幅の修正

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({
 }>) {
 	return (
 		<div
-			className="mx-4 mb-4 p-2 mt-[200px] w-full bg-input/30 border-input z-10  shadow-xs dark:bg-background  dark:border "
+			className="mx-4 mb-4 p-2 mt-[200px] w-full bg-input/30 border-input z-10 shadow-xs dark:bg-background dark:border"
 			style={{
 				height: "calc(100vh - 50px)",
 				width: "calc(100vw - 32px)",
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,7 +2,7 @@ import { Link } from "@/lib/viewTransition/Link";
 
 const Page = () => {
 	return (
-		<ul>
+		<ul className="w-[300px] sm:w-[500px] flex justify-center">
 			<li>
 				<Link className="z-10" href="/posts/welcome">
 					Welcome
